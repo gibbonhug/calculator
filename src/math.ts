6b5853ab70e9664b903720e427ea225eee5f3955
gibbonhug <71813760+gibbonhug@users.noreply.github.com>
2022-05-30 12:50:27 -0400

    Concatenate function changed on TS file instead of JS file

--- src/math.ts
+++ src/math.ts
@@ -41,7 +41,7 @@ function operate(operator: string, num1: number, num2: number) {
 // ex: 4, 5 returns '45' (as a number)
 function concatenate(digit1: number, digit2: number) {
     let numStr = ("" + digit1 + digit2);
-    return parseInt(numStr);
+    return Number(numStr);
 }
 
 
